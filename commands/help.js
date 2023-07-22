ed4ecd9fd4d9ce46fad9eed07c8ac610b2f28d22
Mohd-sharique79 <103924028+Mohd-sharique79@users.noreply.github.com>
2023-07-22 15:55:40 +0530

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+function help(){
+    console.log(`
+    These are some myCLI commands used in various situations:
+
+        1- node main.js tree <path>
+        2- node main.js organize <path>
+        3- node main.js help
+    `);
+}
+
+// let myname = "Mohammad Saif Khan";
+
+module.exports = {
+    //key value pair
+    help:help
+    // me:myname
+
+} 
